7da45f385eb3621d7367e3a6daab934a35420a9b
Sangeeta <sangeetaswainlucky@gmail.com>
2025-11-28 19:39:47 +0530

    image delete

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,7 @@
 import bannerThimb from "./banner-thumb.png";
-import book from "./book.jpg";
 import booksBanner from "./books-banner.jpg";
 import libaryLogo from "./libary-logo.jpg";
 import populaBooks from "./popular-books.jpg";
 import vector from "./vector.jpg";
 
-export { bannerThimb, book, booksBanner, libaryLogo, populaBooks, vector };
+export { bannerThimb, booksBanner, libaryLogo, populaBooks, vector };
