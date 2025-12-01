b8fb91f90d2b377bc44b6d3b0c9fbed8648fbfbc
Sangeeta <sangeetaswainlucky@gmail.com>
2025-12-01 17:51:40 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import store from "./utils/Store";
+import store from "./utils/store";
 
 function App() {
   return (
